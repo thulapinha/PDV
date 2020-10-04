3d77ca7a7a905276c861a273f384e3d15ef2a0c5
thulapinha <thulapinha@gmail.com>
2020-10-04 12:17:58 -0400

    Inicio Projeto PDV

---
+++ src/Classe/JavaSistemaPDV.java
@@ -0,0 +1,15 @@
+package Classe;
+
+import Formularios.frmLogin;
+import java.util.Locale;
+
+public class JavaSistemaPDV {
+
+public static void main(String[] args) {
+    
+    frmLogin login = new frmLogin();
+    login.setLocationRelativeTo(null);//abrir a janela no centro
+    login.setVisible(true);
+}
+
+}
